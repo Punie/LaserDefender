6d9945dc80a4809b58bfae0a25803312099db706
Hugo Saracino <hugo.saracino@gmail.com>
2016-02-16 15:34:50 +0100

    Restricted the player's position

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,13 +4,32 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 	public float m_Speed = 10f;
+	public float m_Padding = 1f;
 
+	float m_xMin;
+	float m_xMax;
+
+
+	void Start ()
+	{
+		float distance = transform.position.z - Camera.main.transform.position.z;
+
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1f, 0f, distance));
+
+		m_xMin = leftmost.x + m_Padding;
+		m_xMax = rightmost.x - m_Padding;
+	}
 
 	void Update ()
 	{
 		if (Input.GetKey (KeyCode.LeftArrow))
-			transform.position += new Vector3 (-m_Speed * Time.deltaTime, 0f, 0f);
-		else if (Input.GetKey(KeyCode.RightArrow))
-			transform.position += new Vector3 (m_Speed * Time.deltaTime, 0f, 0f);
+			transform.position += Vector3.left * m_Speed * Time.deltaTime;
+		else if (Input.GetKey (KeyCode.RightArrow))
+			transform.position += Vector3.right * m_Speed * Time.deltaTime;
+
+		float x = Mathf.Clamp (transform.position.x, m_xMin, m_xMax);
+
+		transform.position = new Vector3 (x, transform.position.y, transform.position.z);
 	}
 }
