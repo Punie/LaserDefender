5c069c8e429b9d850e185d931fa65086b3e585a2
Hugo Saracino <hugo.saracino@gmail.com>
2016-02-16 15:17:53 +0100

    Fixed player controller

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,31 +5,12 @@ public class PlayerController : MonoBehaviour
 {
 	public float m_Speed = 10f;
 
-	private Rigidbody2D m_Rigidbody;
-	private float m_HorizontalInputValue;
-	private float m_VerticalInputValue;
-
-	void Awake ()
-	{
-		m_Rigidbody = GetComponent<Rigidbody2D> ();
-	}
 
 	void Update ()
 	{
-		m_HorizontalInputValue = Input.GetAxis ("Horizontal");
-		m_VerticalInputValue = Input.GetAxis ("Vertical");
-	}
-
-	void FixedUpdate ()
-	{
-		Move ();
-	}
-
-	void Move ()
-	{
-		Vector2 horizontalMovement = transform.right * m_HorizontalInputValue * m_Speed * Time.deltaTime;
-		Vector2 verticalMovement = transform.up * m_VerticalInputValue * m_Speed * Time.deltaTime;
-
-		m_Rigidbody.MovePosition (m_Rigidbody.position + horizontalMovement + verticalMovement);
+		if (Input.GetKey (KeyCode.LeftArrow))
+			transform.position += new Vector3 (-m_Speed * Time.deltaTime, 0f, 0f);
+		else if (Input.GetKey(KeyCode.RightArrow))
+			transform.position += new Vector3 (m_Speed * Time.deltaTime, 0f, 0f);
 	}
 }
